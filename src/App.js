87e4e6bbe040974643a03a1b7ac667358875ce66
Michael <michael.barry.email@gmail.com>
2021-02-15 21:08:30 -0700

    box for one person done. multiple people next

--- src/App.js
+++ src/App.js
@@ -31,8 +31,30 @@ class App extends Component {
     this.state = {
       input: "",
       imgUrl: "",
+      box: {},
     };
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    // console.log(image, width, height, clarifaiFace);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+    console.log(box);
+  };
 
   onInputChange = (e) => {
     this.setState({ input: e.target.value });
@@ -43,14 +65,10 @@ class App extends Component {
 
     app.models
       .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then((response) => {
-        console.log(
-          response.outputs[0].data.regions[0].region_info.bounding_box
-        );
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+      .then((response) =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch((err) => console.log(err));
   };
 
   render() {
@@ -64,7 +82,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecgonition imgUrl={this.state.imgUrl} />
+        <FaceRecgonition imgUrl={this.state.imgUrl} box={this.state.box} />
       </div>
     );
   }
--- src/components/FaceRecgonition/FaceRecgonition.js
+++ src/components/FaceRecgonition/FaceRecgonition.js
@@ -1,11 +1,26 @@
 import React from "react";
 import "./FaceRecgonition.css";
 
-const FaceRecgonition = ({ imgUrl }) => {
+const FaceRecgonition = ({ imgUrl, box }) => {
   return (
     <div className="center ma">
-      <div className="absolute mt2 img-container">
-        <img src={imgUrl} alt="Rendered Image" width="500px" height="auto" />
+      <div className="absolute mt2 center">
+        <img
+          id="inputimage"
+          src={imgUrl}
+          alt="Rendered Image"
+          width="500px"
+          height="auto"
+        />
+        <div
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol,
+          }}
+        ></div>
       </div>
     </div>
   );
